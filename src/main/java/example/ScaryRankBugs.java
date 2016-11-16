6fa419aad3d21207a7041fbf83fdfe9612a79714
tim_cheng <tim_cheng@mail.gss.com.tw>
2016-11-16 10:52:02 +0800

    add FindBug example

---
+++ src/main/java/example/ScaryRankBugs.java
@@ -0,0 +1,67 @@
+package com.javacodegeeks.example.findBugsPluginExample;
+
+/**
+ * 
+ * Scary Rank Bugs Sample
+ * 
+ */
+
+public class ScaryRankBugs {
+
+	/** The check str value. */
+
+	private String checkStrValue = null;
+
+	/**
+	 * 
+	 * The Constructor.
+	 * 
+	 */
+
+	public ScaryRankBugs() {
+
+		this.checkStrValue = "SomeValue";
+
+	}
+
+	/**
+	 * 
+	 * Execute some conditions.
+	 * 
+	 */
+
+	private void executeSomeConditions() {
+
+		if ("SomeValue".equals(this.checkStrValue)) {
+
+			// Condition 1
+
+		} else if ("SomeValue".equals(this.checkStrValue)) {
+
+			// Condition 2
+
+		}
+
+	}
+
+	/**
+
+     * Incorrect assignment in if condition.
+
+     */
+
+    private static void incorrectAssignmentInIfCondition() {
+
+        boolean value = false;
+
+        if (value = true) {
+
+            //do Something
+
+        } else {
+
+            //else Do Something
+
+        }
+
+    }
